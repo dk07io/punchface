df0d868e11d3080c64d89cda2be1922b0f0742fb
jhc <coombsjh@gmail.com>
2015-03-03 23:18:00 -0500

    added Sorcery gem which automatically created a user model

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,38 +6,38 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
-#   def show
-# 	  @projects = Projects.find(params[:id])
-#   end
+  def show
+	  @projects = Projects.find(params[:id])
+  end
 
 
-#   def new
-#     @project = Project.new
-#   end
+  def new
+    @project = Project.new
+  end
 
-#   def create
-#     @project = Project.new(project_params)
+  def create
+    @project = Project.new(project_params)
 
-#     if @project.save
-#       redirect_to projects_url
-#     else
-#       render 'new'
-#     end
-#   end
+    if @project.save
+      redirect_to projects_url
+    else
+      render 'new'
+    end
+  end
 
-#   def update
-#   	@project =Project.find(params[:id])
-#   end
+  def update
+  	@project =Project.find(params[:id])
+  end
 
-#   def destroy
-#   	@project =Project.find(params[:id])
-#   	@project.destroy
-#   	redirect_to project_path
-#   end
+  def destroy
+  	@project =Project.find(params[:id])
+  	@project.destroy
+  	redirect_to project_path
+  end
 
 
-#   private
-#   def project_params
-#     params.require(:project).permit(:name, :description, rewards_attributes: [:amount, :description, :_destroy])
-#   end
+  private
+  def project_params
+    params.require(:project).permit(:name, :description, rewards_attributes: [:amount, :description, :_destroy])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303202205) do
+ActiveRecord::Schema.define(version: 20150304041449) do
 
   create_table "pledges", force: :cascade do |t|
     t.integer  "amount"
@@ -37,5 +37,15 @@ ActiveRecord::Schema.define(version: 20150303202205) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",            null: false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
