0865b479a7042abdae1560095af1de5b6862ce2e
Daniel Kim <dk07io@Daniels-MacBook-Pro.local>
2015-03-06 15:53:21 -0500

    Added rewards controller and routes, as well as views for new and show.

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,2 +1,32 @@
 class RewardsController < ApplicationController
+  
+  # def index
+  # end
+
+  def show
+    @reward = Reward.find(params[:id]);
+  end
+
+  def new
+    @reward = Reward.new
+  end
+
+  def create
+    @reward = Reward.new(reward_params)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def reward_params
+    params.require(:reward).permit(:description, :pledge_limit, :amount)
+  end
+
 end
