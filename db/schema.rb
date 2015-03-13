e30e20bb3f40bb093c222d629634a76fe6ac5f05
Daniel Kim <dk07io@Daniels-MacBook-Pro.local>
2015-03-13 16:33:15 -0400

    Carried out rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150304213919) do
+ActiveRecord::Schema.define(version: 20150313202125) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "comment"
+    t.integer  "project_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pledges", force: :cascade do |t|
     t.integer  "amount"
